8151be8c457bc7c98a3f283be05525b390ab2e89
gregorwolf <gregor.wolf@gmail.com>
2017-11-03 13:32:17 +0000

    Align to https://www.sap.com/developer/tutorials/ci-best-practices-fiori-sapcp.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,93 +1,10 @@
-//jshint node:true
+module.exports = function (grunt) {
+    'use strict';
+    grunt.loadNpmTasks('@sap/grunt-sapui5-bestpractice-build');
 
-"use strict";
-
-module.exports = function(grunt) {
-
-	var ui5version = ""; //jshint ignore:line
-	var uirepo     = "sapui5.hana.ondemand.com"; //jshint ignore:line
-
-	// measures the time each task takes
-	require("time-grunt")(grunt);
-
-	// Project configuration.
-	grunt.initConfig({
-
-		// Configuration to be run (and then tested).
-		openui5_preload: { //jshint ignore:line
-			component: {
-				options: {
-					resources: {
-						cwd: "webapp", // path to app root folder
-						src: [
-						  "**/*.js",
-						  "**/*.fragment.html",
-						  "**/*.fragment.json",
-						  "**/*.fragment.xml",
-						  "**/*.view.html",
-						  "**/*.view.json",
-						  "**/*.view.xml",
-						  "**/*.properties"
-						],
-						prefix: "de/linuxdozent/gittest/publicui" // namespace prefix (in case the namespace is not already in folder structure like sap/ui/core/**)
-					},
-					compress: true,
-					dest: "webapp"
-				},
-				components: true
-			}
-		},
-
-		// validate JS files
-		jshint: {
-			files: [
-				"Gruntfile.js",
-				"src/main/webapp/**/*.js",
-				"!src/main/webapp/util/**",
-				"!src/main/webapp/*-preload.js"
-			],
-			options: {
-				jshintrc: ".jshintrc"
-			}
-		},
-
-		//validate XML files (XML Views)
-		xml_validator: {				//jshint ignore:line
-			your_target: {			  //jshint ignore:line
-				src: ["src/main/webapp/**/*.xml"]
-			},
-		},
-
-		//watch for changed files and reload browser
-		watch: {
-			livereload: {
-				options: {
-					livereload: 35729,
-					keepAlive:true
-				},
-				tasks: ["validate"],
-				files: ["<%= jshint.files %>", "<%= xml_validator.your_target.src %>"]
-			},
-		}
-
-	});
-
-	grunt.loadNpmTasks("grunt-openui5");
-	grunt.loadNpmTasks("grunt-contrib-connect");
-	grunt.loadNpmTasks("grunt-xml-validator");
-	grunt.loadNpmTasks("grunt-contrib-jshint");
-	grunt.loadNpmTasks("grunt-contrib-watch");
-
-	grunt.registerTask("build", [ "openui5_preload"]);
-
-	grunt.registerTask("validate", [
-		"jshint",
-		"xml_validator"]);
-
-	grunt.registerTask("default", [
-		"jshint",
-		"xml_validator",
-		"connect:server",
-		"watch"]);
-
+    grunt.registerTask('default', [
+        'lint',
+        'clean',
+        'build'
+    ]);
 };
